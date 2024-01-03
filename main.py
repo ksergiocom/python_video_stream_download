cdee6b533f2014a48bcf60c0acba031b0abea325
ksergio <113699828+ksergiocom@users.noreply.github.com>
2024-01-03 14:03:45 +0100

    modificacion

---
+++ main.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+import requests
+from tqdm import tqdm
+
+def descargar_video(url, nombre_archivo, cookies):
+    # Configurar los encabezados para incluir las cookies
+    headers = {
+        'Cookie': cookies
+    }
+
+    # Realizar la solicitud HTTP para descargar el video, incluyendo las cookies
+    with requests.get(url, headers=headers, stream=True) as respuesta:
+        # Verificar si la solicitud fue exitosa
+        if respuesta.status_code == 200:
+            # Obtener la longitud total del archivo
+            total_length = int(respuesta.headers.get('content-length'))
+
+            # Abrir un archivo para guardar el contenido del video
+            with open(nombre_archivo, 'wb') as archivo, tqdm(
+                    desc=nombre_archivo,
+                    total=total_length,
+                    unit='iB',
+                    unit_scale=True,
+                    unit_divisor=1024,
+                ) as barra:
+                for chunk in respuesta.iter_content(chunk_size=1024):
+                    tamaño = archivo.write(chunk)
+                    barra.update(tamaño)
+            print(f"Video descargado: {nombre_archivo}")
+        else:
+            print(f"Error al descargar el video: {respuesta.status_code}")
+
+# URL del stream de video
+url_stream = "https://media.codew.io/FromGoogleDrive/streams/214964039325993220267591.mp4"
+# https://media.codew.io/FromGoogleDrive/play.html?id=streams/214964039325993220267591.mp4&playOrder=vod
+
+# Nombre del archivo donde se guardará el video
+nombre_archivo = f"video_descargado_{datetime.now()}.mp4"
+
+# Cookie(s) requerida(s) para la solicitud
+cookies = "nombre_cookie=valor_cookie; otra_cookie=otro_valor"
+
+descargar_video(url_stream, nombre_archivo, cookies)
---
+++ no_progress.py
@@ -0,0 +1,24 @@
+import requests
+
+def descargar_video(url, nombre_archivo):
+    # Realizar la solicitud HTTP para descargar el video
+    respuesta = requests.get(url, stream=True)
+
+    # Verificar si la solicitud fue exitosa
+    if respuesta.status_code == 200:
+        # Abrir un archivo para guardar el contenido del video
+        with open(nombre_archivo, 'wb') as archivo:
+            for chunk in respuesta.iter_content(chunk_size=1024 * 1024):
+                if chunk:  # filtrar los keep-alive chunks
+                    archivo.write(chunk)
+        print(f"Video descargado: {nombre_archivo}")
+    else:
+        print("Error al descargar el video")
+
+# URL del stream de video
+url_stream = "https://media.codew.io/FromGoogleDrive/streams/934985876046602330532523.mp4"
+
+# Nombre del archivo donde se guardará el video
+nombre_archivo = "video_descargado.mp4"
+
+descargar_video(url_stream, nombre_archivo)
